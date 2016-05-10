b23685751e7b5fbb32d066905c5103cf1ed1ec14
Niwpan <niwpan.t@gmail.com>
2016-05-10 11:14:23 +0700

    contact

---
+++ contact.php
@@ -0,0 +1,61 @@
+<?php
+        include 'session.php';
+        include 'connectdb.php';
+?>
+<!DOCTYPE html>
+<html>
+    
+    <?php include 'head.php'; ?>
+    
+    <body>
+        <!-- Fixed navbar -->
+        <?php include 'header.php'; ?>
+        
+        <!-- content start -->    
+        <main>
+           <nav class="text-center">
+            <ul class="pagination pagination-sm">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="text-center">
+                            <h3 class="text-danger"><span class="glyphicon glyphicon-phone"> ติดต่อเรา</h3>
+                        </div>
+                    </div>
+                </div>
+             </div>
+            </ul>
+            </nav>
+           
+            <hr>
+            <br>
+            
+            <div class="container">
+                <div class="row">
+                     <div class="col-md-12">
+                                   
+                        
+                        
+                             <h4 class=" text-muted glyphicon glyphicon-comment">  Facebook : Facebook.com</h4><br><br>
+                      
+                       
+                            <h4 class=" text-muted glyphicon glyphicon-phone"> Tel : 098-xxxxxxx</h4><br><br>
+                       
+                         
+                            <h4 class="text-muted glyphicon glyphicon-envelope"> E-mail : niwpan.suk@gmail.com </h4><br>
+                       
+                        
+                                                                
+                    </div>
+                </div>
+            </div>
+            
+        </main>
+        
+        
+         <?php include 'footer.php'; ?>
+        
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
